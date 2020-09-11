37c03990807ab8e2475a08cb57e1a7976cf53261
ДМ. Па <qtckpuhdsa@gmail.com>
2020-09-11 21:10:28 +0300

    -

--- numba_vec_parallel.py
+++ numba_vec_parallel.py
@@ -10,11 +10,39 @@ Created on Fri Sep 11 13:51:01 2020
 import numpy as np
 import math
 import cmath
-from numba import jit
+from numba import jit, prange
 
 @jit(nopython = True, fastmath = True)
 def GaborWavelet(omega, t, Gabor_coef):
     return 0.3251520240633*math.sqrt(omega)*cmath.exp(complex(-0.5*Gabor_coef*(t*omega*0.187390625129278)**2, omega*t))
+
+
+# @jit(parallel = True)
+# def DWT_signal(ut, a, b, t0, AA, BB, TT, omega0, Gabor_coef):
+
+#     h_step = t0[1]-t0[0]
+    
+#     psi_t = np.empty(TT, dtype = np.complex128)
+#     Wab = np.empty((BB, AA), dtype = np.complex128)
+    
+#     for j in prange(AA):
+#         tmp = np.empty(BB)
+#         for i in range(BB):
+#             for k in range(TT):
+
+#                 t_cur=(t0[k]-b[i])/a[j]               
+#                 psi_t[k] = GaborWavelet(omega0, t_cur, Gabor_coef).conjugate()
+
+#             f_psi = psi_t * ut
+                 
+#             tmp[i] = 0.5*(f_psi[0]+f_psi[-1]) + np.sum(f_psi[2:TT-1])
+            
+#         Wab[:,j] = tmp
+     
+#     Wab = np.multiply(Wab, h_step/np.sqrt(a))
+    
+#     return Wab
+
 
 
 @jit(parallel = True)
@@ -25,7 +53,7 @@ def DWT_signal(ut, a, b, t0, AA, BB, TT, omega0, Gabor_coef):
     psi_t = np.empty(TT, dtype = np.complex128)
     Wab = np.empty((BB, AA), dtype = np.complex128)
     
-    for j in range(AA):
+    for j in prange(AA):
         for i in range(BB):
             for k in range(TT):
 
@@ -33,11 +61,13 @@ def DWT_signal(ut, a, b, t0, AA, BB, TT, omega0, Gabor_coef):
                 psi_t[k] = GaborWavelet(omega0, t_cur, Gabor_coef).conjugate()
 
             f_psi = psi_t * ut
-            
                  
-            Wab[i,j] =  0.5*(f_psi[0]+f_psi[-1]) + np.sum(f_psi[2:TT-1])
-        
+            Wab[i,j] = 0.5*(f_psi[0]+f_psi[-1]) + np.sum(f_psi[2:TT-1])
+
+     
     Wab = np.multiply(Wab, h_step/np.sqrt(a))
     
     return Wab
+
+
 
--- test.py
+++ test.py
@@ -24,9 +24,9 @@ import numba_vec_parallel
 # start data
 
 a = np.arange(1,30,0.5)
-b= np.arange(0,50,0.5)
+b = np.arange(0,50,0.5)
 
-t = np.arange(0,101)
+t = np.arange(0,101, 1)
 ut = np.sin(2*math.pi/50 * t) + 100*np.cos(0.4*t)/(t*t+1)
 
 omega = 1
@@ -43,22 +43,22 @@ Gabor_coef = 8
 
 #Wab = strong_vectorization.DWT_signal(ut, a, b, t, a.size, b.size, t.size, 1, 8)
 
-#Wab = numba_just.DWT_signal(ut, a, b, t, a.size, b.size, t.size, 1, 8)
+Wab = numba_just.DWT_signal(ut, a, b, t, a.size, b.size, t.size, 1, 8)
 
 #Wab = numba_strong.DWT_signal(ut, a, b, t, a.size, b.size, t.size, 1, 8)
 
 Wab = numba_vectorization.DWT_signal(ut, a, b, t, a.size, b.size, t.size, 1, 8)
 
-#Wab = numba_vec_parallel.DWT_signal(ut, a, b, t, a.size, b.size, t.size, 1, 8)
+Wab = numba_vec_parallel.DWT_signal(ut, a, b, t, a.size, b.size, t.size, 1, 8)
 
 
-#%timeit translation_only.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
-#%timeit light_vectorization.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
-#%timeit strong_vectorization.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
-#%timeit numba_strong.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
-#%timeit numba_just.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
-#%timeit numba_vectorization.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
-#%timeit numba_vec_parallel.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
+%timeit translation_only.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
+%timeit light_vectorization.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
+%timeit strong_vectorization.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
+%timeit numba_strong.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
+%timeit numba_just.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
+%timeit numba_vectorization.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
+%timeit numba_vec_parallel.DWT_signal(ut, a, b, t, a.size, b.size, t.size, omega, Gabor_coef)
 
 
 heatmap(Wab.real, a, b,)
